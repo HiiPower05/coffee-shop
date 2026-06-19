1fd8ac9474dd808f8b17853220f8ae2152f84a9c
HiiPower05 <fordarkseid13@gmail.com>
2026-06-19 16:01:50 +0200

    Add models and made migrations.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django import render 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    # The order you want the category to display on the page
+    display_order = models.IntegerField(default=0) 
+
+    def __str__(self):
+        return self.name
+    
+
+class MenuItem(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, help_text="Brief description of ingredients")
+    price = models.DecimalField(max_digits=6, decimal_digits=2) 
+    is_available = models.BooleanField(default=True) 
+
+    def __str__(self):
+        return self.name
+    
+class Location(models.Model):
+    branch_name = models.CharField(max_length=100) 
+    address = models.CharField(max_length=255)
+    operating_hours = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.branch_name
